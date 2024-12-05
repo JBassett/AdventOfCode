191e72f717b5b3b75929c5b1c2f06257d5b83636
Justin Bassett <Bassett.JustinT@gmail.com>
2024-12-05 18:46:10 -0500

    Day 5, Part 1

---
+++ src/main/kotlin/com/github/jbassett/y2024/d05/Part1.kt
@@ -0,0 +1,40 @@
+package com.github.jbassett.y2024.d05
+
+import java.io.InputStream
+
+class Part1 {
+    fun solve(input: InputStream): Int {
+        val orderMap = mutableMapOf<Int, MutableList<Int>>()
+        val updates = mutableListOf<List<Int>>()
+        input.bufferedReader().readLines().forEach { line ->
+            if (line.contains("|")) {
+                val parts = line.split("|")
+                val key = parts[0].trim().toInt()
+                val f = orderMap.getOrPut(key) { mutableListOf() }
+                f.add(parts[1].trim().toInt())
+            } else if (line.contains(",")) {
+                updates.add(line.split(",").map { it.toInt() })
+            }
+        }
+
+        return updates.map { update ->
+            val sorted = update.sortedWith { o1, o2 ->
+                val f1 = orderMap[o1]
+                val f2 = orderMap[o2]
+
+                if (f1?.contains(o2) == true) {
+                    return@sortedWith -1
+                } else if (f2?.contains(o1) == true) {
+                    return@sortedWith 1
+                } else {
+                    return@sortedWith 0
+                }
+            }
+            if (sorted == update) {
+                update[update.size / 2]
+            } else {
+                0
+            }
+        }.sum()
+    }
+}
